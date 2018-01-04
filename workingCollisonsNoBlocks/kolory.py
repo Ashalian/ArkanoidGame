7e07069bb17b561ba06613e80c3c4043b5699b61
Ashalian <mateuszficek295@gmail.com>
2018-01-04 13:54:30 +0100

    workingCollisonsNoBlocks

---
+++ workingCollisonsNoBlocks/kolory.py
@@ -0,0 +1,8 @@
+import sys
+class Colors:
+    def __init__(self, game):
+
+        self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+        self.red = (255, 0, 0)
+        self.lightGreen = (32, 182, 152)
